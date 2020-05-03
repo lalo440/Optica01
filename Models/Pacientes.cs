d1aafb9bfa87c15692827d053cce4435385b0ec1
Juan Eduardo Hernández Mandujano <juan.hernadez246@tesh.edu.mx>
2020-05-03 14:25:33 -0500

    img se agrego

--- Models/Pacientes.cs
+++ Models/Pacientes.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
 
 namespace Optica01.Models
 {
